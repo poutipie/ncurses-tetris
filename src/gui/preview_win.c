8066e77fd1c28e4233e9eb3bbe2f2967c6cfb333
Pietari Poutiainen <pietari.poutiainen@gmail.com>
2022-05-29 14:55:23 +0300

    Add initial version of tetris block preview window

--- src/gui/preview_win.c
+++ src/gui/preview_win.c
@@ -6,7 +6,7 @@ void PreviewWin_init(PreviewWin** self) {
 }
 void _PreviewWin_create_win(PreviewWin* self) {
 
-    self->m_height = 5;
+    self->m_height = 6;
     self->m_width = 10;
 
 	int starty = ((LINES - (TETRIS_ROWS + 2)) / 2) + self->m_height + 1;
@@ -24,5 +24,31 @@ void PreviewWin_destroy(PreviewWin** self) {
 }
 
 void PreviewWin_draw(PreviewWin* self, GameState* h_game_state) {
+
+    TetrisBlock* preview_block = h_game_state->h_next_tetris_block;
+
+    for (int i = 1; i < self->m_width - 1; ++i) {
+        for (int j = 1; j < self->m_height - 1; ++j) {
+            wmove(self->m_win, j, i);
+            wprintw(self->m_win, " ");
+        }
+    }
+
+    size_t offset_x = (self->m_width / 2) - 1;
+    size_t offset_y = (self->m_height / 2) - 1;
+
+    wattron(self->m_win, COLOR_PAIR(preview_block->m_color_scheme));
+    for (int i = 0; i < preview_block->m_blk_size; ++i) {
+
+        int draw_row = preview_block->m_blk_coords[i].y + offset_y;
+        int draw_column = preview_block->m_blk_coords[i].x + offset_x;
+
+        if (draw_row <= self->m_width || draw_column <= self->m_height) {
+            wmove(self->m_win, draw_row, draw_column);
+            wprintw(self->m_win, "*");
+        }
+    }
+    wattroff(self->m_win, COLOR_PAIR(preview_block->m_color_scheme));
+
     wrefresh(self->m_win);
 }
